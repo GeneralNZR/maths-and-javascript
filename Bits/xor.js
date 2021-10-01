9a059d2a46239e30ff5e1c2d928e6ddd6729cae7
GeneralMoeWan <zeitoun.noam@gmail.com>
2021-10-01 20:31:46 +0200

    add xor.js

---
+++ Bits/xor.js
@@ -0,0 +1,21 @@
+/**
+ * Algorithme pour échanger les valeurs de deux variables 
+ * sans utiliser la variable temporaire qui est normalement requise
+ * avec l'opérateur " ^ " OU exclusif (XOR).
+ * @param {number} x - Premier nombre
+ * @param {number} y - Second nombre
+ * @returns {Array} Tableau contenant les deux nombres
+ */
+const xor = (x, y) => [x ^ x ^ y, y ^ y ^ x];
+
+// Test
+console.log(xor(0, 1)); // [1, 0]
+
+/* ------------------- 
+ │ x   │ y   │ x ^ y │
+ ├─────┼─────┼───────┤
+ │ 0   │ 0   │   0   │
+ │ 1   │ 1   │   0   │
+ │ 0   │ 1   │   1   │
+ │ 1   │ 0   │   1   │ 
+ ------------------- */
