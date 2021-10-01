4d079d5738240f6ccd7eef5885e7f2370caa8ac7
GeneralMoeWan <zeitoun.noam@gmail.com>
2021-10-01 23:02:33 +0200

    add sommeConsecutive.js

---
+++ Bits/sommeConsecutive.js
@@ -0,0 +1,14 @@
+/**
+ * Verifie si un nombre peut être exprimé en somme de deux ou plusieurs nombres entiers positifs consecutifs.
+ * @param {number} int - Le nombre à tester.
+ * @returns {boolean} - True si le nombre peut être exprimé en somme de deux ou plusieurs nombres entiers positifs consecutifs, false sinon.
+ */
+const sommeConsecutive = int => !!(int & (int - 1));
+
+// Test
+console.log(sommeConsecutive(10)); // true, car 10 = 1 + 9
+console.log(sommeConsecutive(64)); // false
+
+/*
+L'opérateur !! permet de transformer un objet en booléen.
+*/
